b1dc74cc3e8754d70026e2e5ac461bf457c673c1
Element Green <element@kymorphia.com>
2025-03-25 15:01:23 -0600

    Convert tabs to spaces

--- gid-gtk3-leak-test/app.d
+++ gid-gtk3-leak-test/app.d
@@ -11,51 +11,51 @@ import std.stdio : writeln;
 
 class HelloWorld : ApplicationWindow
 {
-	this(Application application)
-	{
-		super(application);
-		setTitle("giD");
-		setBorderWidth(10);
-		add(new Label("Hello World"));
+  this(Application application)
+  {
+    super(application);
+    setTitle("giD");
+    setBorderWidth(10);
+    add(new Label("Hello World"));
 
-		showAll();
+    showAll();
 
-		import glib.global : timeoutAddSeconds;
-		import glib.types : PRIORITY_DEFAULT, SOURCE_CONTINUE;
-		import core.memory : GC;
-		import std.stdio : writeln;
-		timeoutAddSeconds(PRIORITY_DEFAULT, 1, () { writeln("GC.collect"); GC.collect; return SOURCE_CONTINUE; });
-	}
+    import glib.global : timeoutAddSeconds;
+    import glib.types : PRIORITY_DEFAULT, SOURCE_CONTINUE;
+    import core.memory : GC;
+    import std.stdio : writeln;
+    timeoutAddSeconds(PRIORITY_DEFAULT, 1, () { writeln("GC.collect"); GC.collect; return SOURCE_CONTINUE; });
+  }
 }
 
 class TestLeak : ApplicationWindow
 {
-	this(Application application)
-	{
-		super(application);
-		setTitle("TestLeak");
-		setBorderWidth(10);
+  this(Application application)
+  {
+    super(application);
+    setTitle("TestLeak");
+    setBorderWidth(10);
 
-		auto hbox = new HBox(false, 6);
-		add(hbox);
+    auto hbox = new HBox(false, 6);
+    add(hbox);
 
     auto button = Button.newWithLabel("Click Me");
     button.connectClicked(&buttonClicked);
     hbox.add(button);
 
-		showAll();
-	}
+    showAll();
+  }
 
   void buttonClicked()
   {
     writeln("Button clicked!");
-		close;
+    close;
   }
 }
 
 int main(string[] args)
 {
-	auto application = new Application("org.gid.demo.helloworld", ApplicationFlags.FlagsNone);
-	application.connectActivate(delegate void(GioApplication app) { new HelloWorld(application); new TestLeak(application); });
-	return application.run(args);
+  auto application = new Application("org.gid.demo.helloworld", ApplicationFlags.FlagsNone);
+  application.connectActivate(delegate void(GioApplication app) { new HelloWorld(application); new TestLeak(application); });
+  return application.run(args);
 }
--- gid-gtk4-leak-test/app.d
+++ gid-gtk4-leak-test/app.d
@@ -12,53 +12,53 @@ import std.stdio : writeln;
 
 class HelloWorld : ApplicationWindow
 {
-	this(Application application)
-	{
-		super(application);
-		setTitle("giD");
-		setChild(new Label("Hello World"));
+  this(Application application)
+  {
+    super(application);
+    setTitle("giD");
+    setChild(new Label("Hello World"));
 
-		import glib.global : timeoutAddSeconds;
-		import glib.types : PRIORITY_DEFAULT, SOURCE_CONTINUE;
-		import core.memory : GC;
-		import std.stdio : writeln;
-		timeoutAddSeconds(PRIORITY_DEFAULT, 1, () { writeln("GC.collect"); GC.collect; return SOURCE_CONTINUE; });
-	}
+    import glib.global : timeoutAddSeconds;
+    import glib.types : PRIORITY_DEFAULT, SOURCE_CONTINUE;
+    import core.memory : GC;
+    import std.stdio : writeln;
+    timeoutAddSeconds(PRIORITY_DEFAULT, 1, () { writeln("GC.collect"); GC.collect; return SOURCE_CONTINUE; });
+  }
 }
 
 class TestLeak : ApplicationWindow
 {
-	this(Application application)
-	{
-		super(application);
-		setTitle("TestLeak");
+  this(Application application)
+  {
+    super(application);
+    setTitle("TestLeak");
 
-		auto hbox = new Box(Orientation.Vertical, 6);
-		setChild(hbox);
+    auto hbox = new Box(Orientation.Vertical, 6);
+    setChild(hbox);
 
     auto button = Button.newWithLabel("Click Me");
     button.connectClicked(&buttonClicked);
     hbox.append(button);
-	}
+  }
 
   void buttonClicked()
   {
     writeln("Button clicked!");
-		close;
+    close;
   }
 }
 
 int main(string[] args)
 {
-	auto application = new Application("org.gid.demo.helloworld", ApplicationFlags.FlagsNone);
+  auto application = new Application("org.gid.demo.helloworld", ApplicationFlags.FlagsNone);
 
-	application.connectActivate(delegate void(GioApplication app) {
-		auto helloWorld = new HelloWorld(application);
-		auto testLeak = new TestLeak(application);
+  application.connectActivate(delegate void(GioApplication app) {
+    auto helloWorld = new HelloWorld(application);
+    auto testLeak = new TestLeak(application);
 
-		helloWorld.present;
-		testLeak.present;
-	});
+    helloWorld.present;
+    testLeak.present;
+  });
 
-	return application.run(args);
+  return application.run(args);
 }
